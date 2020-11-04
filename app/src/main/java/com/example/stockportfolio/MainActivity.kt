0197ff00d84ae9cabe39e0ac78ea895613f11f5c
samim <samimgooogle@gmail.com>
2020-11-04 01:56:26 -0500

    first commit

---
+++ app/src/main/java/com/example/stockportfolio/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.stockportfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+    }
+
+   
+}
+
