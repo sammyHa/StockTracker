0661b68e35570ada463e54fba5290240c3c0d484
samim <samimgooogle@gmail.com>
2020-11-06 05:05:21 -0500

    first commit

--- app/src/main/java/com/example/stockportfolio/fragments/AddFragment.kt
+++ app/src/main/java/com/example/stockportfolio/fragments/AddFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.stockportfolio.R
+import java.math.BigDecimal
 
 
 class AddFragment : Fragment() {
+    val price: EditText? =null
+    val quantity: EditText? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -19,7 +26,24 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+
+
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        val quantity = view?.findViewById<EditText>(R.id.quantity)
+        val price = view?.findViewById<EditText>(R.id.price)
+        val totalCost = view?.findViewById<TextView>(R.id.total_cost)
+
+        (view?.findViewById<Button>(R.id.btn_save)).also {
+
+            it?.setOnClickListener({
+
+                val result = quantity?.text.toString().toDouble() * price?.text.toString().toDouble()
+
+            })
+        }
+        return view
     }
 }
+
+
+
